556cb4124f6fab4dcc73c11ba257b31bda8a005b
lionthai <takw2477>
2018-06-26 12:31:54 +0700

    how to use bootstrap and Vue and Jquery 1.install node js 2. cmd to workjob 3.npm install 4.npm run development 5.<link href="/css/app.css" rel > 6.<script src"/js/app.js" ></script> 7.you can use it

--- resources/views/themes/welcome/main.blade.php
+++ resources/views/themes/welcome/main.blade.php
@@ -4,10 +4,8 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <script src="{{ asset('js/jquery.min.js') }}"></script>
-        <script src="{{ asset('js/bootstrap.js') }}"></script>
-        <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet">
+        <script src="/js/app.js" ></script>
+        <link rel="stylesheet" href="/css/app.css" crossorigin="anonymous" >
 
         <!-- Fonts -->
 
